744a15062e07b0f8423271272aed9d8de699612a
Evan Willhite <evan@fourkitchens.com>
2017-09-21 13:47:22 -0500

    switch up sift map script

--- scripts/sift-map.sh
+++ scripts/sift-map.sh
@@ -2,11 +2,10 @@
 # Install Sift-Map mapper into node_modules
 
 # Determine location of node_modules
-# if standalone
-if [ -d ./node_modules ]
+if [ -d ../../node_modules ]
 then
   cd ./node_modules && ln -s ../mapper/siftmap
-elif [ -d ../../node_modules ]
+elif [ -d ./node_modules ]
 then
   cd ../../node_modules && ln -s ./sift/mapper/siftmap
 fi
