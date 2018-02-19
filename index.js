9819bc620bc959e396de4d30bcb69048c3871708
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-02-19 12:25:24 -0600

    KFTP-7: removed incorrect index change

--- index.js
+++ index.js
@@ -7,14 +7,3 @@ exports = module.exports = sift;
 function sift (options) {
   wpt.wpt(options)
 }
-
-sift({
-  key: 'A.82807ee5a9e7dce9e3a3cdee30f01567',
-  tests: [
-    {
-      name: 'Google.com',
-      url: 'http://google.com',
-      type: 'homepage'
-    }
-  ]
-});
