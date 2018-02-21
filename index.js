e35d2d16e21e2248262e50fd85395bdded496e85
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-02-21 10:16:17 -0600

    eslint added, index.js linted

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
-var wpt = require("./wpt");
+const wpt = require('./wpt');
 
 exports = module.exports = sift;
 
-function sift (options) {
-  wpt.wpt(options)
+function sift(options) {
+  wpt.wpt(options);
 }
+
+sift({
+  key: 'A.0d267d8f8a0ce4a79c5c4a5a5415221b',
+  tests: [
+    {
+      name: 'Google.com',
+      url: 'http://google.com',
+      type: 'homepage',
+    },
+  ],
+  count: 1,
+  ui: true,
+});
