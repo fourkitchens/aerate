7d77e100e2527144a3258cb27fb78a5ae3e7b430
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-02-22 13:54:04 -0600

    fix index.js

--- index.js
+++ index.js
@@ -5,18 +5,3 @@ const wpt = require('./wpt');
 function sift(options) {
   wpt.wpt(options);
 }
-
-exports = module.exports = sift; // eslint-disable-line
-
-sift({
-  key: 'A.0d267d8f8a0ce4a79c5c4a5a5415221b',
-  tests: [
-    {
-      name: 'Google.com',
-      url: 'http://google.com',
-      type: 'homepage',
-    },
-  ],
-  count: 1,
-  ui: true,
-});
