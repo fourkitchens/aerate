6d76566e58d119319876c7eba3cfe9f357c149a8
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-02-22 15:12:33 -0600

    index.js

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const wpt = require('./wpt');
 function sift(options) { // eslint-disable-line
   wpt.wpt(options);
 }
+
+sift({
+  key: 'A.0d267d8f8a0ce4a79c5c4a5a5415221b',
+  tests: [
+    {
+      name: 'Google.com',
+      url: 'http://google.com',
+      type: 'homepage',
+    },
+    {
+      name: 'Yahoo.com',
+      url: 'https://www.yahoo.com/',
+      type: 'homepage',
+    },
+  ],
+  count: 1,
+  ui: true,
+});
