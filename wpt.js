eaa9fcb6915f20a5acb537df379b0cf4adc1f443
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-06-06 10:26:13 -0500

    support for custom URI

--- wpt.js
+++ wpt.js
@@ -25,9 +25,10 @@ const wptRun = (options, ngrok) => {
     .fetch({
       key: options.key,
       tests: options.tests,
-      connection: options.connection || 'Mobile LTE',
+      connection: options.connection || 'Cable',
       count: options.count || 9,
-      uri: options.uri || 'www.webpagetest.org'
+      uri: options.uri || 'www.webpagetest.org',
+      location: options.location || ''
     })
     .then(result => {
       // For each test.
