3d88737ab1b8f059f99a797d644967339494ccdb
Patrick Coffey <patrickcoffey48@gmail.com>
2021-11-05 14:10:51 -0500

    feat: add husky install to postInstall

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: ['main'],
-  repositoryUrl: 'git@github.com:StewardBot/starter-typescript.git',
+  repositoryUrl: 'git@github.com:fourkitchens/aerate.git',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
